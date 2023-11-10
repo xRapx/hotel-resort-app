6ec0b2342c54cd335e1272eec8755eb611964aab
xRapx <huynhminhquan2706@gmail.com>
2023-11-10 18:07:00 +0700

    Update 3
    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title="Luxurious rooms " subtile="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          Ours Rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
